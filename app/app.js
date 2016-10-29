ebc8401e44d58ff6df240c494b329559bda28a47
Rifhan Akram <rifhan.akram1@gmail.com>
2016-10-29 21:46:26 +0530

    - Add initial bot stop

--- app/app.js
+++ app/app.js
@@ -1,28 +1,105 @@
 'use strict';
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const PlaylistQueue = require('./util/playlist');
-const Playlist = new PlaylistQueue();
-let count = 0;
-app.use(express.static('client'));
-app.use(express.static('public'));
-app.get('/api/add', (req,res) => {
-    res.status(200).json({
-        temp : Playlist.add(count++)
-    })
+
+var express = require('express');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+var server = express();
+server.listen(process.env.port || process.env.PORT || 3000, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
-app.get('/api/get', (req,res) => {
-    res.status(200).json({
-        temp : Playlist.get()
-    })
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-app.get('/api/all', (req,res) => {
-    res.status(200).json({
-        temp : Playlist.all(),
-        queue : Playlist.que()
-    })
+var bot = new builder.UniversalBot(connector);
+server.use(express.static('client'));
+server.use(express.static('public'));
+server.post('/api/messages', connector.listen());
+
+
+//=========================================================
+// Activity Events
+//=========================================================
+
+bot.on('conversationUpdate', function (message) {
+    // Check for group conversations
+    if (message.address.conversation.isGroup) {
+        // Send a hello message when bot is added
+        if (message.membersAdded) {
+            message.membersAdded.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Hello everyone!");
+                    bot.send(reply);
+                }
+            });
+        }
+
+        // Send a goodbye message when bot is removed
+        if (message.membersRemoved) {
+            message.membersRemoved.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Goodbye");
+                    bot.send(reply);
+                }
+            });
+        }
+    }
 });
-app.listen(port, () => {
-    console.log('server listening on port ', port);
-})
+
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+            .address(message.address)
+            .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
+
+bot.on('deleteUserData', function (message) {
+    // User asked to delete their data
+});
+
+
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+
+
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
+
+
+bot.dialog('/', [
+    function (session) {
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("Microsoft Bot Framework")
+            .text("Your bots - wherever your users are talking.")
+            .images([
+                builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.send("Hi... I'm the Microsoft Bot Framework demo bot for Skype. I can show you everything you can use our Bot Builder SDK to do on Skype.");
+        session.beginDialog('/help');
+    },
+    function (session, results) {
+        // Display menu
+        session.beginDialog('/menu');
+    },
+    function (session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
+    }
+]);
