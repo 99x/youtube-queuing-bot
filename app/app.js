7ac10b07d5cb8572ec8b3903d6eb3cbefcf3ef2d
Rifhan Akram <rifhan.akram1@gmail.com>
2016-10-30 06:44:56 +0530

    - minor changes

--- app/app.js
+++ app/app.js
@@ -17,69 +17,70 @@ const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-const bot = new builder.UniversalBot(connector);
+const bot = new builder.UniversalBot(connector, {
+   localizerSettings: {
+   botLocalePath: "./locale",
+   defaultLocale: "en"
+ }
+ });
 server.use(express.static('client'));
 server.use(express.static('public'));
 server.post('/api/messages', connector.listen());
 
 
-//=========================================================
-// Activity Events
-//=========================================================
+bot.on('conversationUpdate', function (message) {
+    // Check for group conversations
+    if (message.address.conversation.isGroup) {
+        // Send a hello message when bot is added
+        if (message.membersAdded) {
+            message.membersAdded.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Hello everyone!");
+                    bot.send(reply);
+                }
+            });
+        }
 
-// bot.on('conversationUpdate', function (message) {
-//     // Check for group conversations
-//     if (message.address.conversation.isGroup) {
-//         // Send a hello message when bot is added
-//         if (message.membersAdded) {
-//             message.membersAdded.forEach(function (identity) {
-//                 if (identity.id === message.address.bot.id) {
-//                     var reply = new builder.Message()
-//                         .address(message.address)
-//                         .text("Hello everyone!");
-//                     bot.send(reply);
-//                 }
-//             });
-//         }
+        // Send a goodbye message when bot is removed
+        if (message.membersRemoved) {
+            message.membersRemoved.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Goodbye");
+                    bot.send(reply);
+                }
+            });
+        }
+    }
+});
 
-//         // Send a goodbye message when bot is removed
-//         if (message.membersRemoved) {
-//             message.membersRemoved.forEach(function (identity) {
-//                 if (identity.id === message.address.bot.id) {
-//                     var reply = new builder.Message()
-//                         .address(message.address)
-//                         .text("Goodbye");
-//                     bot.send(reply);
-//                 }
-//             });
-//         }
-//     }
-// });
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+            .address(message.address)
+            .text("Hello %s... Thanks for adding me. Post your youtube links here and i will and them to the playlist ");
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
 
-// bot.on('contactRelationUpdate', function (message) {
-//     if (message.action === 'add') {
-//         var name = message.user ? message.user.name : null;
-//         var reply = new builder.Message()
-//             .address(message.address)
-//             .text("Hello %s... Thanks for adding me. Post your youtube links here and i will and them to the playlist ");
-//         bot.send(reply);
-//     } else {
-//         // delete their data
-//     }
-// });
-
-// bot.on('deleteUserData', function (message) {
-//     // User asked to delete their data
-// });
+bot.on('deleteUserData', function (message) {
+    // User asked to delete their data
+});
 
 
-// // Anytime the major version is incremented any existing conversations will be restarted.
-// bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+// Anytime the major version is incremented any existing conversations will be restarted.
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
 
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hello Please Send me a youtube link and i will add to our playlist");
+        builder.Prompts.text(session, "Hello... What's your name?");
     },
     function (session, results) {
         session.userData.name = results.response;
