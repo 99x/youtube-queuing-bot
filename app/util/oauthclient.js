36226a118a87b94e7ee99295474cb3379e61455c
CharithW <yashoda.charith10@gmail.com>
2016-10-31 17:10:27 +0530

    minor changes

--- app/util/oauthclient.js
+++ app/util/oauthclient.js
@@ -1,49 +1,9 @@
-// 'use strict'
 
-
-// //Oauth2 flow for using googleapis
-
-// var google = require('googleapis'); 
-// var OAuth2Client = google.auth.OAuth2; 
-// var spawn = require('child_process').spawn;
-// var url = require('url'); 
-// var querystring = require('querystring');
-// var auth = require('../auth'); 
-
-
-
-// function handler(requets , response ,callback) {
-//     var self = this ; 
-//     var qs = querystring.parse(url.parse(request.url).query);
-//     self.OAuth2Client.getToken(qs.code, function(err , tokens){
-//         if(err){
-//             console.log('token error' + err);
-//         }
-        
-//     })
-// }
-
-
-
-// Copyright 2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//SampleClient googleapis-nodejs-client 
 
 'use strict';
 
-/**
- * This is used by several samples to easily provide
- * an oauth2 workflow.
- */
+
 
 var google = require('googleapis');
 var OAuth2Client = google.auth.OAuth2;
@@ -82,22 +42,24 @@ function SampleClient (options) {
   self.isAuthenticated = false;
   this._options = options || { scopes: [] };
 
-  // create an oAuth client to authorize the API call
+  
   this.oAuth2Client = new OAuth2Client(
     secrets.web.client_id,
     secrets.web.client_secret,
     secrets.web.redirect_uris[0]
   );
 
-  // Open an http server to accept the oauth callback. In this
-  // simple example, the only request to our webserver is to
-  // /callback?code=<code>
+ 
   this._authenticate = function (scopes, callback) {
-    // grab the url that will be used for authorization
+   
     self.authorizeUrl = self.oAuth2Client.generateAuthUrl({
       access_type: 'offline',
       scope: scopes.join(' ')
     });
+  
+  
+  
+  
     var server = http.createServer(function (request, response) {
       callOnce(function () {
         handler.call(self, request, response, server, callback);
--- app/util/youtubea.js
+++ app/util/youtubea.js
@@ -1,22 +1,4 @@
-// //Authentication done via youtube-api 
 
-
-// const Youtube = require('youtube-api')
-//       , 
-
-
-// Copyright 2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 'use strict';
 
@@ -30,7 +12,6 @@ var youtube = google.youtube({
   auth: sampleClient.oAuth2Client
 });
 
-// a very simple example of searching for youtube videos
 function getCategory (id) {
   youtube.search.list({
     part: 'id,snippet',
@@ -42,12 +23,10 @@ function getCategory (id) {
     if (data) {
       console.log(util.inspect(data, false, null));
     }
-    process.exit();
+   
   });
 }
 
 var scopes = [
   'https://www.googleapis.com/auth/youtube'
 ];
-
-sampleClient.execute(scopes, runSamples);
